6047bb30da4e16870ee61e74c9e0c93e71050582
pedro <pedrohperetto@gmail.com>
2025-08-28 13:08:58 -0300

    feat: creating new component Hero, importing it's images and adding to the ProductPage and routes

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,32 @@
+import { BannerProducts, GamesInfos } from './styles'
+import bannerHogwarts from '../../assets/images/fundo_hogwarts.png'
+import Tag from '../Tag'
+import Button from '../Button'
+
+const Hero = () => (
+    <BannerProducts style={{ backgroundImage: `url(${bannerHogwarts})` }}>
+        <div className="container">
+            <div>
+                <Tag>RPG</Tag>
+                <Tag>PS5</Tag>
+            </div>
+
+            <GamesInfos>
+                <h2>Hogwarts' Legacy</h2>
+                <p>
+                    <span>De R$ 250,00</span>
+                    Por R$ 190,00
+                </p>
+                <Button
+                    type="button"
+                    variant="primary"
+                    title="Clique aqui para adicionar este jogo ao carrinho"
+                >
+                    Adicionar ao carrinho
+                </Button>
+            </GamesInfos>
+        </div>
+    </BannerProducts>
+)
+
+export default Hero
---
+++ src/pages/ProductPage/index.tsx
@@ -0,0 +1,14 @@
+import { useParams } from 'react-router-dom'
+import Hero from '../../components/Hero'
+
+const ProductPage = () => {
+    const { id } = useParams()
+
+    return (
+        <>
+            <Hero />
+        </>
+    )
+}
+
+export default ProductPage
