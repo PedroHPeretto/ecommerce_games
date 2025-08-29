ae3ee1d4d5c49659ab444d223c556b04d52ab3e1
pedro <pedrohperetto@gmail.com>
2025-08-29 14:20:57 -0300

    feat: adding pop up images and videos to the Gallery and making new functions on the ProductPage

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,127 @@
+import Section from '../Section'
+import {
+    GalleryItem,
+    GalleryItems,
+    ImageAction,
+    Modal,
+    ModalContent
+} from './styles'
+import imageGallery from '../../assets/images/gallery_image.png'
+import imagePlay from '../../assets/images/play.png'
+import imageZoom from '../../assets/images/zoom.png'
+import ImageClose from '../../assets/images/fechar.png'
+import { useState } from 'react'
+
+interface GalleryMock {
+    type: 'image' | 'video'
+    img?: string
+    url: string
+    id?: number
+}
+
+const mock: GalleryMock[] = [
+    {
+        id: 1,
+        type: 'image',
+        img: imageGallery,
+        url: imageGallery
+    },
+    {
+        id: 2,
+        type: 'image',
+        img: imageGallery,
+        url: imageGallery
+    },
+    {
+        id: 3,
+        type: 'video',
+        img: imageGallery,
+        url: 'https://www.youtube.com/embed/1O6Qstncpnc?si=DdfRqIcjY80pKX81'
+    },
+    {
+        id: 4,
+        type: 'video',
+        img: imageGallery,
+        url: 'https://www.youtube.com/embed/1O6Qstncpnc?si=DdfRqIcjY80pKX81'
+    }
+]
+
+interface ModalState extends GalleryMock {
+    isVisible: boolean
+}
+
+const Gallery = () => {
+    const [modal, setModal] = useState<ModalState>({
+        isVisible: false,
+        type: 'image',
+        url: ''
+    })
+
+    const closeModal = () => {
+        setModal({
+            isVisible: false,
+            type: 'image',
+            url: ''
+        })
+    }
+
+    const getMediaIcon = (item: GalleryMock) => {
+        if (item.type === 'image') return imageZoom
+        return imagePlay
+    }
+
+    return (
+        <>
+            <Section title="Galeria" bg="black">
+                <GalleryItems>
+                    {mock.map((media) => (
+                        <GalleryItem
+                            key={media.id}
+                            onClick={() => {
+                                setModal({
+                                    isVisible: true,
+                                    type: media.type,
+                                    img: media.img,
+                                    url: media.url
+                                })
+                            }}
+                        >
+                            <img src={media.img} alt="Mídia do jogo" />
+                            <ImageAction>
+                                <img
+                                    src={getMediaIcon(media)}
+                                    alt="Clique para maximizar a mídia"
+                                />
+                            </ImageAction>
+                        </GalleryItem>
+                    ))}
+                </GalleryItems>
+            </Section>
+            <Modal className={modal.isVisible ? 'visible' : ''}>
+                <ModalContent className="container">
+                    <header>
+                        <h4>Hogwarts Legacy</h4>
+                        <img
+                            src={ImageClose}
+                            alt="Fechar"
+                            onClick={() => closeModal()}
+                        />
+                    </header>
+                    {modal.type === 'image' ? (
+                        <img src={modal.url} />
+                    ) : (
+                        <iframe src={modal.url} />
+                    )}
+                </ModalContent>
+                <div
+                    className="overlay"
+                    onClick={() => {
+                        closeModal()
+                    }}
+                ></div>
+            </Modal>
+        </>
+    )
+}
+
+export default Gallery
---
+++ src/components/Gallery/styles.ts
@@ -0,0 +1,90 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+
+export const GalleryItems = styled.ul`
+    display: flex;
+`
+
+export const GalleryItem = styled.li`
+    margin-right: 16px;
+    position: relative;
+
+    > img {
+        border: 2px solid ${cores.branco};
+        border-radius: 8px;
+        height: 150px;
+        width: 150px;
+        object-fit: cover;
+    }
+`
+
+export const ImageAction = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    opacity: 0;
+
+    &:hover {
+        opacity: 100%;
+        transition: opacity 0.5s ease;
+    }
+`
+
+export const Modal = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    display: none;
+    justify-content: center;
+    align-items: center;
+
+    &.visible {
+        display: flex;
+    }
+
+    .overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.7);
+    }
+`
+
+export const ModalContent = styled.div`
+    max-width: 960px;
+    position: relative;
+    z-index: 1;
+
+    header {
+        display: flex;
+        margin-bottom: 24px;
+        justify-content: space-between;
+
+        h4 {
+            font-weight: bold;
+            font-size: 18px;
+        }
+    }
+
+    img,
+    iframe {
+        display: block;
+        max-width: 100%;
+    }
+
+    iframe {
+        width: 100%;
+        height: 480px;
+    }
+`
